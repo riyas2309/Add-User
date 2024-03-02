69b690e4a25dd14924e70d0d70bca90dc40ce311
Mohamed Riyas. Ashraf Ali <riyassucces@gmail.com>
2024-03-02 08:33:52 +0530

    dompleted deign

--- Add User Details/src/App.jsx
+++ Add User Details/src/App.jsx
@@ -1,7 +1,10 @@
+import InputCard from "./Components/InputCard";
+import ResultCard from "./Components/ResultCard";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <InputCard />
+      <ResultCard />
     </>
   );
 }
--- Add User Details/src/Components/InputCard.jsx
+++ Add User Details/src/Components/InputCard.jsx
@@ -2,11 +2,23 @@ import React from "react";
 
 function InputCard() {
   return (
-    <div>
+    <div className=" mt-[5rem] p-5 bg-[#FFFF] flex items-center w-[50rem] rounded-lg">
       <form action="">
-        <div>
-          <label htmlFor="">UserName</label>
+        <div className="flex flex-col">
+          <label htmlFor="" className="pb-2 font-bold">
+            UserName
+          </label>
+          <input type="text" className="border border-gray-400 w-[47rem]" />
         </div>
+        <div className="flex flex-col pb-5">
+          <label htmlFor="" className="pb-2 font-bold">
+            Age (years)
+          </label>
+          <input type="text" className="border border-gray-400 w-[47rem] " />
+        </div>
+        <button type="submit" className="p-2 bg-purple-500">
+          Add User
+        </button>
       </form>
     </div>
   );
