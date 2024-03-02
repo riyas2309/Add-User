87eb41761ad81b019cede48c302dd1563e6af722
Mohamed Riyas. Ashraf Ali <riyassucces@gmail.com>
2024-03-02 08:11:21 +0530

    initial commit

---
+++ Add User Details/src/Components/InputCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function InputCard() {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="">UserName</label>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default InputCard;
